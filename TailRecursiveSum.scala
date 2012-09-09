426b22ed5f1479e9449cd6a788e32d456962ce69
cgopalan <cgopalan@Chandrakant-Gopalans-MacBook.local>
2012-09-09 17:13:08 -0400

    Changed spaces

--- TailRecursiveSum.scala
+++ TailRecursiveSum.scala
@@ -1,12 +1,12 @@
 object FirstClassFunctions {
-  def sum(f: Int => Int)(a: Int, b:Int): Int = {
-    def iter(a: Int, result: Int): Int =
-      if (a > b) result
-      else iter(a + 1, f(a) + result)
-    iter(a,0)
-  }
-  
-  def main(args: Array[String]) {
-    println("Sum is: " + sum(x => x)(1,10))
-  }
+  def sum(f: Int => Int)(a: Int, b:Int): Int = {
+    def iter(a: Int, result: Int): Int =
+      if (a > b) result
+      else iter(a + 1, f(a) + result)
+    iter(a,0)
+  }
+  
+  def main(args: Array[String]) {
+    println("Sum is: " + sum(x => x)(1,10))
+  }
 }
