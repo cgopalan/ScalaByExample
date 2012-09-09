abfdac64c7279f01a0a5298e76fdb092d9a5ce8c
cgopalan <cgopalan@Chandrakant-Gopalans-MacBook.local>
2012-09-09 17:08:19 -0400

    Changed spaces

--- TailRecursiveFactorial.scala
+++ TailRecursiveFactorial.scala
@@ -1,8 +1,8 @@
 object TailRecursiveFactorial {
-   def factorial(n: Int, product: Int): Int = 
-      if (n == 1) product else factorial(n - 1, n * product)
-   
-   def main(args: Array[String]) {
-   println("Factorial of 8 is: " + factorial(8,1))
-   }
+   def factorial(n: Int, product: Int): Int = 
+      if (n == 1) product else factorial(n - 1, n * product)
+   
+   def main(args: Array[String]) {
+   println("Factorial of 8 is: " + factorial(8,1))
+   }
 }
